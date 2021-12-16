4d8a3c6319911ae083d11f3be0bf2e54f201763d
ori davidi <oridavidi100@gmail.com>
2021-12-16 15:38:03 +0200

    a

--- routers/blog.js
+++ routers/blog.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { getAllBlogs, newBlog, deleteBlog, updateLikes } = require('../controllers/blog');
-router.put('/api/blogs/update', updateLikes);
-router.get('/api/blogs', getAllBlogs);
-router.delete('/api/blogs', deleteBlog);
-router.post('/api/blogs', newBlog);
+router.put('/update', updateLikes);
+router.get('/', getAllBlogs);
+router.delete('/', deleteBlog);
+router.post('/', newBlog);
 module.exports = router;
